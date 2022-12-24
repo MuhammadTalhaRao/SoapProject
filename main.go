a7423f3d0c28054be17a07fb0387e2cfbf29ab8a
Muhammad Talha <31736957+Dervaish19@users.noreply.github.com>
2022-12-24 20:39:57 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/api/soap", handleRequest)
+
+	router.Run(":7000")
+}
+
+func handleRequest(ctx *gin.Context) {
+	var request Request
+	err := ctx.ShouldBindXML(&request)
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+	}
+
+	resp, err := createResponse()
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+	}
+
+	ctx.Data(http.StatusOK, "Content-Type: text/html; charset=utf-8", resp)
+}
+
+func createResponse() ([]byte, error) {
+	var resp bytes.Buffer
+
+	template, err := template.ParseFiles("response.html")
+	if err != nil {
+		return resp.Bytes(), err
+	}
+
+	// hard coded responce
+	template.Execute(&resp, GetPriceResponse{
+		Price: 1.6,
+	})
+
+	return resp.Bytes(), nil
+}
+
+type Request struct {
+	Envalop  xml.Name `xml:"Envelope"`
+	GetPrice GetPrice `xml:"Body>GetPrice"`
+}
+
+type GetPrice struct {
+	Item string `xml:"Item"`
+}
+
+type GetPriceResponse struct {
+	Price float64 `xml:"Price"`
+}
